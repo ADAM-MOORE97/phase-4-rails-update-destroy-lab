ba458882aa061113a29dcd1be26f276e038e89ed
Adam <adammoore118@outlook.com>
2021-11-24 13:29:07 -0500

    commit tree 61559c29788c55cc17b6e65dc67922ebe981c5ce

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found 
   # GET /plants
   def index
     plants = Plant.all
@@ -8,7 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -17,10 +17,26 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
 
   private
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+  def resource_not_found
+    render json: {error: "Plant not found"}, status: :not_found
   end
 end
